b14f225a228dbdc53261a9d1842818e0ce0ae83e
Francisco Sevilla <fsevilla@gmail.com>
2018-05-07 16:18:59 -0500

    added multer middleware in server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var multer = require('multer');
 
-app.use(express.static(__dirname) + '/dist');
+// app.use(express.static(__dirname) + '/dist');
+
+app.use(multer({
+  dest: path.join(__dirname, '/dist')
+}).any());
 
 app.listen(process.env.PORT || 8080);
 
