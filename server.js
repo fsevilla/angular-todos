98f0c73ff235ef3ee37f5ec8d9cde48b963608df
Francisco Sevilla <fsevilla@gmail.com>
2018-05-08 15:09:14 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
+
+// Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname) + '/dist', function (req, res, next) {
 	console.log('Use dist: ', Date.now());
 	next();
 });
 
-app.listen(process.env.PORT || 8080);
-
-
-app.get('/', function(req, res) {
+app.get('/*', function(req,res) {
 	res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 console.log('Server is listening!!!');
